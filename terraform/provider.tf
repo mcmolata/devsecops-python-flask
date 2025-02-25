ba9fd67387769961fa8bfbcc4ede2b24df25a728
mcmolata <mcmolata@gmail.com>
2025-02-25 14:17:53 +1300

    replace the var to private

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "proxmox" {
   pm_api_url          = "https://192.227.162.136:8006/api2/json"
-  pm_api_token_id     = "root@pam!op" 
-  pm_api_token_secret = "7879b698-a672-486b-98c0-e59ff997f1aa"     
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret 
   pm_tls_insecure     = true
 }
