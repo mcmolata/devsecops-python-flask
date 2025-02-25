88adeec66b20a73707d8bd9e6083f43e13248c48
mcmolata <mcmolata@gmail.com>
2025-02-25 17:19:58 +1300

    fix kill apt

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "proxmox_vm_qemu" "terraform_vm" {
   # remote command
   provisioner "remote-exec" {
     inline = [
-      "sudo kill -9 $(sudo lsof -t /var/lib/dpkg/lock-frontend) 2>/dev/null || true",
+      "sudo kill -9 $(ps -ef | grep apt-get | grep -v grep | awk '{print $2}')",
       "sudo apt-get install -y docker.io",
       "sudo curl -L \"https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
