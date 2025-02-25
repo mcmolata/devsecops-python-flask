ee63d18b6edecad396b950fc4bfa89c4f512ca6c
mcmolata <mcmolata@gmail.com>
2025-02-25 14:19:19 +1300

    modify the url to var

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://192.227.162.136:8006/api2/json"
+  pm_api_url          = var.url
   pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret 
   pm_tls_insecure     = true
